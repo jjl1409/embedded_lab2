1a6ce3306a5091054057fefd630cc03579c4ece5
jjl1409 <amsjlam@gmail.com>
2023-02-27 17:05:34 -0500

    Error fixing part1

--- fbputchar.c
+++ fbputchar.c
@@ -19,11 +19,6 @@
 #include <linux/fb.h>
 
 #define FBDEV "/dev/fb0"
-    struct winsize w;
-    ioctl(0, TIOCGWINSZ, &w);
-
-    printf ("lines %d\n", w.ws_row);
-    printf ("columns %d\n", w.ws_col);
 #define FONT_WIDTH 8
 #define FONT_HEIGHT 16
 #define BITS_PER_PIXEL 32
@@ -32,6 +27,8 @@ struct fb_var_screeninfo fb_vinfo;
 struct fb_fix_screeninfo fb_finfo;
 unsigned char *framebuffer;
 static unsigned char font[];
+int max_rows;
+int max_cols;
 
 /*
  * Open the framebuffer to prepare it to be written to.  Returns 0 on success
@@ -39,6 +36,13 @@ static unsigned char font[];
  */
 int fbopen()
 {
+  struct winsize w;
+  ioctl(0, TIOCGWINSZ, &w);
+
+  printf ("lines %d\n", w.ws_row);
+  printf ("columns %d\n", w.ws_col);
+  max_rows = w.ws_row;
+  max_cols = w.ws_col;
   int fd = open(FBDEV, O_RDWR); /* Open the device */
   if (fd == -1) return FBOPEN_DEV;
 
@@ -106,9 +110,8 @@ void fbputchar(char c, int row, int col)
 
 void fbline(char c, int row)
 {
-	int cols = w.ws_col;
 	int i;
-	for (i = 0; i < cols; i++)
+	for (i = 0; i < max_cols; i++)
 	{
 		fbputchar(c, row, i);
 	}
@@ -116,9 +119,8 @@ void fbline(char c, int row)
 
 void fbclear()
 {
-	int rows = w.ws_row;
 	int row;
-	for (row = 0; row < rows; row++)
+	for (row = 0; row < max_rows; row++)
 	{
 		fbline(' ', row);
 	}
